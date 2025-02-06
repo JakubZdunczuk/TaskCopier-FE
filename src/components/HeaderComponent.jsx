5b41c4639fb4d7a34fc0e8017b3a053391d40e93
jakubzdunczuk <jakub.zdunczuk@gmail.com>
2025-02-06 21:30:25 +0100

    first commit

---
+++ src/components/HeaderComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+    return {
+        <div>
+            <header>
+                <nav className="nav navbar-dark">
+                    <a className="navbar-brand" href="http://localhost:3000">Task Copy App</a>
+                </nav>
+            </header>
+        </div>
+    }
+}
+
+export default Header
